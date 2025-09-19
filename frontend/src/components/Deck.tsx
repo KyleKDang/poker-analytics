0f936a736512c6f10bcbecc186c591d4b54ae507
Kyle Dang <kylekd1@uci.edu>
2025-09-18 23:04:08 -0700

    feat: add Deck component to display a scrollable card deck

---
+++ frontend/src/components/Deck.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Card from "./Card";
+
+interface DeckProps {
+  deck: string[];
+}
+
+export default function Deck({ deck }: DeckProps) {
+  return (
+    <div className="flex flex-wrap gap-2 max-h-[200px] overflow-y-auto p-2 border rounded bg-gray-900">
+      {deck.map((c) => (
+        <Card key={c} code={c} id={`deck-${c}`} size={60} />
+      ))}
+    </div>
+  );
+}
