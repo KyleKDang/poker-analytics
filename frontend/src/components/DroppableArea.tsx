24396e4307ea1317a66109cf4dbe00e0baf4dc2d
Kyle Dang <kylekd1@uci.edu>
2025-09-18 23:00:43 -0700

    feat: add DroppableArea component for drag-and-drop cards

---
+++ frontend/src/components/DroppableArea.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useDroppable } from "@dnd-kit/core";
+import Card from "./Card";
+
+interface DroppableAreaProps {
+  id: string;
+  cards: string[];
+  onCardDrop: (cardCode: string) => void;
+  maxCards: number;
+}
+
+export default function DroppableArea({
+  id,
+  cards,
+  onCardDrop,
+  maxCards,
+}: DroppableAreaProps) {
+  const { setNodeRef } = useDroppable({ id });
+
+  return (
+    <div
+      ref={setNodeRef}
+      className="flex min-h-[100px] space-x-2 p-2 border-2 border-dashed rounded bg-gray-800"
+    >
+      {cards.map((c, i) => (
+        <Card key={i} code={c} id={`${id}-${c}`} size={60} />
+      ))}
+      {cards.length === 0 && (
+        <span className="text-gray-400">Drop cards here</span>
+      )}
+    </div>
+  );
+}
